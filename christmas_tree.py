2a78389f866fa46fb8c44dbb872ac22deec119e7
Nurbanu Bursa <50712055+nursuznb@users.noreply.github.com>
2025-12-08 23:44:35 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- christmas_tree.py
+++ christmas_tree.py
@@ -121,10 +121,10 @@ QUESTIONS = {
     8: (
       "Say you were running a linear regression for a dataset but you accidentally duplicated every data point.\n" 
       "What happens to your beta coefficient?\n"
-      "- **A)**  $"\beta = (2X^T X) \, 2X^T y"$\n"
-      "- **B)**  $"\beta = (2X^T X)^{-1} \, 2X^T y"$\n"
-      "- **C)**  $"\beta = (2X^T X)^{-1} $\n"
-      "- **D)**  $"\beta = (2X^T X)^{-1} \, 2X^T"$\n"
+      "- **A)**  $\beta = (2X^T X) \, 2X^T y$\n"
+      "- **B)**  $\beta = (2X^T X)^{-1} \, 2X^T y$\n"
+      "- **C)**  $\beta = (2X^T X)^{-1} $\n"
+      "- **D)**  $\beta = (2X^T X)^{-1} \, 2X^T$\n"
     ),
     9: (
         "“______ is a common method used to handle categorical data in preprocessing.”\n\n"
@@ -569,6 +569,7 @@ if current_day is not None and st.button("Submit"):
 
         # 🔁 AĞACI AYNI ÇALIŞTIRMADA TEKRAR ÇİZDİR
         tree_placeholder.markdown(generate_tree_html(answers), unsafe_allow_html=True)
+
 
 
 
